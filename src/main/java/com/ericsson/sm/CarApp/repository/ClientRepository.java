ffdb7ef9d340bf11496ad476597893006c637a59
milardich <stjepan.milardic1@gmail.com>
2023-01-26 13:55:33 +0100

    getAll() now returns all clients if params firstName and lastName are null

--- src/main/java/com/ericsson/sm/CarApp/repository/ClientRepository.java
+++ src/main/java/com/ericsson/sm/CarApp/repository/ClientRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    @Query("SELECT c FROM Client c WHERE c.firstName = ?1 OR c.lastName = ?2 ORDER BY c.lastName ASC")
+    @Query("SELECT c FROM Client c WHERE (?1 is null OR c.firstName = ?1) AND (?2 is null OR c.lastName = ?2) ORDER BY c.lastName ASC")
     Page<Client> findAllByFirstNameOrLastName(
             @Param("firstName") String firstName,
             @Param("lastName") String lastName,
