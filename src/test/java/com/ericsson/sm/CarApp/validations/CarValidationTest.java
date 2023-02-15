50660a6ccf064b636a3317fede16710e8d0afb04
milardich <stjepan.milardic1@gmail.com>
2023-02-15 15:49:23 +0100

    car validation tests

--- src/test/java/com/ericsson/sm/CarApp/validations/CarValidationTest.java
+++ src/test/java/com/ericsson/sm/CarApp/validations/CarValidationTest.java
@@ -4,11 +4,13 @@ package com.ericsson.sm.CarApp.validations;
 import com.ericsson.sm.CarApp.dto.CarRequestDto;
 import com.ericsson.sm.CarApp.exception.GenericValidationException;
 import com.ericsson.sm.CarApp.repository.CarRepository;
+import com.ericsson.sm.CarApp.repository.CarServiceRepository;
 import com.ericsson.sm.CarApp.validation.CarValidation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -19,6 +21,8 @@ public class CarValidationTest {
 
     @InjectMocks
     private CarValidation carValidation;
+    @Autowired
+    private CarServiceRepository carServiceRepository;
 
     @Test
     void testEmptyColor_throwsGenericValidationException(){
@@ -27,6 +31,32 @@ public class CarValidationTest {
         carRequestDto.setCarType("OPEL_ASTRA");
         carRequestDto.setColor("");
         carRequestDto.setRegistrationMark("WW 222 WW");
+
+        Throwable exception = Assertions.assertThrows(GenericValidationException.class, () -> carValidation.validate(carRequestDto));
+
+        Assertions.assertEquals(GenericValidationException.class, exception.getClass());
+    }
+
+    @Test
+    void testIncorrectRegistrationMarkFormat_throwsGenericValidationException(){
+        CarRequestDto carRequestDto = new CarRequestDto();
+        carRequestDto.setManufactureYear(1999);
+        carRequestDto.setCarType("OPEL_ASTRA");
+        carRequestDto.setColor("Red");
+        carRequestDto.setRegistrationMark("WW 222554 WW");
+
+        Throwable exception = Assertions.assertThrows(GenericValidationException.class, () -> carValidation.validate(carRequestDto));
+
+        Assertions.assertEquals(GenericValidationException.class, exception.getClass());
+    }
+
+    @Test
+    void testInvalidCarType_throwsGenericValidationException(){
+        CarRequestDto carRequestDto = new CarRequestDto();
+        carRequestDto.setManufactureYear(2999);
+        carRequestDto.setCarType("MUSTANG");
+        carRequestDto.setColor("Red");
+        carRequestDto.setRegistrationMark("WW 22554 WW");
 
         Throwable exception = Assertions.assertThrows(GenericValidationException.class, () -> carValidation.validate(carRequestDto));
 
